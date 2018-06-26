ff5d4d821e82b4eed4008f5491119c498db7cfb8
Henry Hellbusch <hhellbusch@gmail.com>
2018-06-26 15:17:06 -0500

    add seatsAvailable end point

--- routes/index.js
+++ routes/index.js
@@ -12,4 +12,12 @@ router.get('/healthz', function(req, res, next) {
   });
 });
 
+// check to see if seats are available
+// for now - just do this randomly
+router.get('/seatsAvailable', function(req, res, next){
+  // false 10% of the time; true 90% of the time
+  let result = Math.random() >= 0.1;
+  res.send(result);
+});
+
 module.exports = router;
